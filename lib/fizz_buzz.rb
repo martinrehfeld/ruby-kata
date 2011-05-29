52902dec15b7eb26c768929b9643f8bcf5bd8c2b
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-05-29 15:22:37 +0200

    more concise version ?!

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,11 +2,11 @@ class FizzBuzz
 
   def initialize
     @results = (1..100).map { |i|
-      if i % 3 == 0 && i % 5 == 0 then 'FizzBuzz'
-      elsif i % 3 == 0 then 'Fizz'
-      elsif i % 5 == 0 then 'Buzz'
-      else i
-      end
+      val = ''
+      val << 'Fizz' if i % 3 == 0
+      val << 'Buzz' if i % 5 == 0
+      val = i       if val.empty?
+      val
     }
   end
 
